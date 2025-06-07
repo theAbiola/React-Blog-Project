bd5e85e04299a7f4cc8f36d819bbfa3181fc9693
Abdulraheem Habib Abiola <iamhabibabiola@gmail.com>
2025-06-07 11:40:12 +0100

    Add single blog fetch functionality

--- frontend/src/components/BlogDetails.jsx
+++ frontend/src/components/BlogDetails.jsx
@@ -1,14 +1,30 @@
 import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch.js";
+import LoadingModal from "./LoadingModal.jsx";
+
 
 const BlogDetails = () => {
 
     const { id } = useParams()
 
+    const { resourceData: blog, isLoading, isError } = useFetch(`http://localhost:3500/posts/${id}`)
+
     return (
         <div className="blog-details">
-            <h2> Blog details - {id} </h2>
+            {isError && <p> Error fetching post, try again, this time with a valid URL or endpoint...</p>}
+            {isLoading && <LoadingModal />}
+            {blog &&
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>{blog.body}</p>
+                    <p style={{
+                        paddingTop: "10px",
+                        paddingBottom: "10px"
+                    }}>Written by: {blog.author}</p>
+                </article>}
+
         </div>
-    );
+    )
 }
 
 export default BlogDetails;
